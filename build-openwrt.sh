0a557d4831bb39cfc1bcf3acfb9defe026e802c3
leonardo.arrighi <leoarry@gmail.com>
2026-05-18 22:24:01 +0100

    added batman mesh support

--- build-openwrt.sh
+++ build-openwrt.sh
@@ -3,7 +3,7 @@
 set -e
 
 VERSION="25.12.3"
-PACKAGES="luci luci-ssl-openssl rsync jq pigz tar iperf3 tcpdump"
+PACKAGES="luci luci-ssl-openssl -wpad-basic-mbedtls wpad-mesh-openssl kmod-batman-adv luci-proto-batman-adv batctl-full rsync jq pigz tar iperf3 tcpdump"
 
 usage() {
     echo "Usage: $0 [-v version] [-p packages]"
